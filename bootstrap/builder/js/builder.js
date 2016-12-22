edd4b3b6118c74a92bc563e15ed58698985904b1
Nilesh <nnmahaja@cs.indiana.edu>
2016-12-22 00:45:06 -0500

    simple website builder

---
+++ bootstrap/builder/js/builder.js
@@ -0,0 +1,13 @@
+$('document').ready(function() {
+    // not sure why this does not work immediately? boostrap?
+
+    /*
+    $('body').click(function() {
+	// do something here like:
+        alert('hey! The body click is working!!!')
+    });
+    */
+    $('#divmain').click(function() {
+	alert('divmain clicked')
+    });
+});
